5286cf96ef5212419bc81d76c8e87fbc33db25d2
wanquan <615861192@qq.com>
2021-04-01 16:42:00 +0800

    提交说明

---
+++ text.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SHVAR='shell变量';
+SHLET="好好学习";
+COMMIT="提交说明";
+
+#echo $SHVAR
+#echo ""运行结果："${SHVAR} ${SHLET}"
+
+# 循环语句
+for item in $(ls ./files); #$(ls ./files)你可以写成 `ls ./files`
+    do
+        echo ·""for输出:"${item}";
+        #$(vim item)
+    done
+
+
+$(git add .)
+$(git commit -m "${COMMIT}")
+$(git push origin main)
